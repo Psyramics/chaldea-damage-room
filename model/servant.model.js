82041ee66e501715c7c751b5f13680eb4c043706
Psyramics <cyberbro85+github@gmail.com>
2018-07-26 14:11:20 -0400

    Restructure this a little

--- model/servant.model.js
+++ model/servant.model.js
@@ -6,16 +6,16 @@
 	  defaults: {
       id: 2,
       level: 1,
-      hp: 0,
-      atk: 0,
-      np: 1,
-      np_interlude: false,
-      skill1: 1,
-      skill1_interlude: false,
-      skill2: 1,
-      skill2_interlude: false,
-      skill3: 1,
-      skill3_interlude: false
+      fouhp: 0,
+      fouatk: 0,
+      nplvl: 1,
+      npstr: 0,
+      skill1lvl: 1,
+      skill1str: 0,
+      skill2lvl: 1,
+      skill2str: 0,
+      skill3lvl: 1,
+      skill3str: 0
 	  },
     constructor: function () {
       Backbone.Model.apply(this, arguments);
@@ -31,10 +31,13 @@
     }
     
     var files = [
-      'saber.json',
+      'servants',
+      'skills',
+      'nps',
+      'curves',
     ];
     for (var i = 0; i < files.length; i++) {
-      jQuery.get('data/'+files[i]).done(function (data, text, jqXHR) {
+      jQuery.get('data/'+files[i]+'.json').done(function (data, text, jqXHR) {
         console.log(data);
       }));
     }
